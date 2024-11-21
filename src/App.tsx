7a290cb9f2dd21026398498889a58a8a874d8a36
RubenMMPS <rmartinmotilla@gmail.com>
2024-11-21 12:17:24 +0100

    add

--- src/App.tsx
+++ src/App.tsx
@@ -12,59 +12,15 @@ const slides = [
 const App = () => {
   return (
     <div>
-      {/* Primer contenedor */}
-      <div className="container">
-        <h1>Barberia</h1>
-        <p>
-          Zlatibor is a mountain of exceptional beauty whose special
-          geographical properties have made this mountain a real gem of western
-          Serbia.
-        </p>
-        <a href="#">Learn more</a>
-      </div>
-
-      <div className="blank"></div>
-
-      {/* Segundo contenedor con elementos */}
-      <div className="container second">
-        <div className="item">
-          <div className="img img-first"></div>
-          <div className="card">
-            <h3>Rock climbing</h3>
-            <p>
-              The goal is to reach the summit of a formation or the endpoint of
-              a usually pre-defined route without falling
-            </p>
-            <a href="#">Learn more</a>
-          </div>
-        </div>
-        <div className="item">
-          <div className="img img-second"></div>
-          <div className="card">
-            <h3>Caving</h3>
-            <p>
-              Exploring underground through networks of tunnels and passageways,
-              which can be natural or artificial.
-            </p>
-            <a href="#">Learn more</a>
-          </div>
-        </div>
-        <div className="item">
-          <div className="img img-third"></div>
-          <div className="card">
-            <h3>Parachuting</h3>
-            <p>
-              Jumping from an aeroplane and falling through the air before
-              opening your parachute.
-            </p>
-            <a href="#">Learn more</a>
-          </div>
-        </div>
-      </div>
-      <div style={{ width: "600px", height: "400px", margin: "0 auto" }}>
+      <div
+        style={{
+          width: "400px",
+          height: "600px",
+          margin: "0 auto",
+        }}
+      >
         <ImageSalider slides={slides} />
       </div>
-      <div className="blank"></div>
     </div>
   );
 };
